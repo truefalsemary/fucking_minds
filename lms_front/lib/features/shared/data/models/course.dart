20446c9cb69b85c42e06b671aafe44477a80145b
Roman Berezkin <DeadSakura777@mail.ru>
2024-08-04 16:26:26 +0300

    Add course model

---
+++ lms_front/lib/features/shared/data/models/course.dart
@@ -0,0 +1,31 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'course.g.dart';
+
+@JsonSerializable()
+class Course {
+  final String id;
+  final String title;
+
+  @JsonKey(name: "start_ts", fromJson: DateTime.parse)
+  final DateTime? startDate;
+
+  @JsonKey(name: "end_ts", fromJson: DateTime.parse)
+  final DateTime? endDate;
+
+  final String description;
+
+  final String authorId;
+
+  Course({
+    required this.id,
+    required this.title,
+    required this.startDate,
+    required this.endDate,
+    required this.description,
+    required this.authorId,
+  });
+
+  factory Course.fromJson(Map<String, dynamic> json) => _$CourseFromJson(json);
+  Map<String, dynamic> toJson() => _$CourseToJson(this);
+}
