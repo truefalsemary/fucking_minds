27c9bbe34b7ae4d9de65aa47ddc0c3f891169c67
Roman Berezkin <DeadSakura777@mail.ru>
2024-08-09 22:54:53 +0300

    Add basic color for borders

--- lms_front/lib/ui_kit/app_colors.dart
+++ lms_front/lib/ui_kit/app_colors.dart
@@ -7,4 +7,7 @@ class AppColors {
   // Foreground colors
   static const yellow = Color(0xffffcc00);
   static const black = Color(0xff221D22);
+
+  // Border colors
+  static const borderColor = Color(0xFFB4BFC6);
 }
