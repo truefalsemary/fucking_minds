890128ca4d0f4a68aabae46119a93b008cda120e
Roman Berezkin <DeadSakura777@mail.ru>
2024-08-09 17:04:13 +0300

    Fix icons paths via AppIcons abstract class, +refactor
    Now _CourseTicket accepts Widget instead of String, so now we can insert anything in there which is more general and reusable solution.

--- lms_front/lib/ui_kit/app_icons.dart
+++ lms_front/lib/ui_kit/app_icons.dart
@@ -4,4 +4,22 @@ class AppIcons {
   static final SvgPicture editPencil = SvgPicture.asset(
     'assets/icons/edit_pencil.svg',
   );
+
+  static final SvgPicture databaseImg = SvgPicture.asset(
+    'assets/images/database.svg',
+    width: 50,
+    height: 50,
+  );
+
+  static final SvgPicture projectImg = SvgPicture.asset(
+    'assets/images/project.svg',
+    width: 50,
+    height: 50,
+  );
+
+  static final SvgPicture snapshotImg = SvgPicture.asset(
+    'assets/images/snapshot.svg',
+    width: 50,
+    height: 50,
+  );
 }
