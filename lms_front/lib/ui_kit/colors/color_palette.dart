f2adba7266db87baaa18ecf944f7e686fbbb0895
Roman Berezkin <DeadSakura777@mail.ru>
2024-08-10 16:05:52 +0300

    Add theme extension for colors and move constant colors to palette

--- lms_front/lib/ui_kit/colors/color_palette.dart
+++ lms_front/lib/ui_kit/colors/color_palette.dart
@@ -1,6 +1,6 @@
-import 'dart:ui';
+import 'package:flutter/material.dart';
 
-class AppColors {
+abstract class ColorPalette {
   // Background colors
   static const backgroundColor = Color(0xffFFFBF5);
 
