5749917a7f618dfddc4c06ae0e7a1c13fda83c7b
golden <dden4929@gmail.com>
2024-08-13 19:59:57 +0300

    feat: add auth

--- service/pg_service_template/src/controllers/authentification/auth_controller.hpp
+++ service/pg_service_template/src/controllers/authentification/auth_controller.hpp
@@ -3,7 +3,5 @@
 #include "../../models/auth/token_model.hpp"
 
 namespace lms_service {
-class AuthController {
-  std::string GetUserIdByToken(const lms_service::Token& token);
-};
+  std::string GetUserIdByToken(const lms_service::Token& token, userver::storages::postgres::ClusterPtr pg_cluster_);
 }  // namespace lms_service
