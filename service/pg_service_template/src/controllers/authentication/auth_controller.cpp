7556feea91eaf9ecaf12555441cac6bf1bfc93ec
eagerbeaver04 <mike.markov04@gmail.com>
2024-08-14 01:44:42 +0300

    fix: replace functions

--- service/pg_service_template/src/controllers/authentication/auth_controller.cpp
+++ service/pg_service_template/src/controllers/authentication/auth_controller.cpp
@@ -1,11 +1,23 @@
 #include "auth_controller.hpp"
 
-#include <userver/storages/postgres/cluster.hpp>
-
 #include "../../models/auth/token_model.hpp"
 
 namespace lms_service {
 namespace authentication {
+
+std::optional<std::string> GetSessionInfo(
+    userver::storages::postgres::ClusterPtr pg_cluster,
+    const userver::server::http::HttpRequest& request) {
+  if (!request.HasHeader(USER_TICKET_HEADER_NAME)) {
+    return std::nullopt;
+  }
+
+  Token token;
+  token.data = request.GetHeader(USER_TICKET_HEADER_NAME);
+  return authentication::GetUserIdByToken(token, pg_cluster);
+}
+
+
 std::optional<std::string> GetUserIdByToken(
     const lms_service::Token& token,
     userver::storages::postgres::ClusterPtr pg_cluster_) 
