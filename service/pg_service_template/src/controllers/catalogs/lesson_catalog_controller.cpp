2aee5bd87fa9bb263b8499c9040d4f113a5bc821
eagerbeaver04 <mike.markov04@gmail.com>
2024-08-14 12:02:37 +0300

    feat: add get handles, add TODO

--- service/pg_service_template/src/controllers/catalogs/lesson_catalog_controller.cpp
+++ service/pg_service_template/src/controllers/catalogs/lesson_catalog_controller.cpp
@@ -7,6 +7,7 @@ namespace lms_service {
 namespace lesson_catalog_controller {
 Lesson createLesson(const lms_service::LessonData& lesson_data,
                     userver::storages::postgres::ClusterPtr pg_cluster_) {
+  //TODO: add author status validation
   auto result = pg_cluster_->Execute(
       userver::storages::postgres::ClusterHostType::kMaster,
       "INSERT INTO Lessons(lesson_title, lesson_description, author_id) "
@@ -17,30 +18,40 @@ Lesson createLesson(const lms_service::LessonData& lesson_data,
   return result.AsSingleRow<Lesson>(userver::storages::postgres::kRowTag);
 }
 
-// userver::storages::postgres::ResultSet getLessons(
-//     userver::storages::postgres::ClusterPtr pg_cluster_) {
-//   return pg_cluster_->Execute(
-//       userver::storages::postgres::ClusterHostType::kMaster,
-//       "SELECT * FROM Lessons");
-// }
+std::vector<Lesson> getLessons(
+    userver::storages::postgres::ClusterPtr pg_cluster_) 
+    {
+  auto result = pg_cluster_->Execute(
+      userver::storages::postgres::ClusterHostType::kMaster,
+      "SELECT * FROM Lessons");
+  return result.AsContainer<std::vector<Lesson>>(
+      userver::storages::postgres::kRowTag);
+}
 
-// userver::storages::postgres::ResultSet getLessonByID(
-//     const std::string& id,
-//     userver::storages::postgres::ClusterPtr pg_cluster_) {
-//   return pg_cluster_->Execute(
-//       userver::storages::postgres::ClusterHostType::kMaster,
-//       "SELECT * FROM Lessons WHERE lesson_id = $1", id);
-// }
+std::optional<Lesson> getLessonByID(
+    const std::string& id,
+    userver::storages::postgres::ClusterPtr pg_cluster_) {
+  auto result = pg_cluster_->Execute(
+      userver::storages::postgres::ClusterHostType::kMaster,
+      "SELECT * FROM Lessons WHERE lesson_id = $1", id);
 
-// userver::storages::postgres::ResultSet updateLessonByID(
+  return result.AsOptionalSingleRow<Lesson>(
+      userver::storages::postgres::kRowTag);
+}
+
+//TODO: is it necessary to check author?
+// std::optional<Lesson> updateLessonByID(
 //     const std::string& id, const lms_service::LessonData& lesson_data,
 //     userver::storages::postgres::ClusterPtr pg_cluster_) {
-//   return pg_cluster_->Execute(
+//   auto result = pg_cluster_->Execute(
 //       userver::storages::postgres::ClusterHostType::kMaster,
 //       "UPDATE Lessons "
 //       " SET title = $1, description = $2 "
 //       "WHERE lesson_id = $3",
 //       lesson_data.title, lesson_data.description, id);
+
+//   return result.AsOptionalSingleRow<Lesson>(
+//       userver::storages::postgres::kRowTag);
 // }
 };  // namespace lesson_catalog_controller
 }  // namespace lms_service
--- service/pg_service_template/src/controllers/catalogs/lesson_catalog_controller.hpp
+++ service/pg_service_template/src/controllers/catalogs/lesson_catalog_controller.hpp
@@ -3,25 +3,27 @@
 #include <userver/storages/postgres/cluster.hpp>
 #include "../../models/lesson/lesson.hpp"
 #include "../../models/lesson/lesson_data.hpp"
+#include <vector>
+#include <optional>
 
 namespace lms_service {
 namespace lesson_catalog_controller {
 Lesson createLesson(const lms_service::LessonData& lesson_data,
                     userver::storages::postgres::ClusterPtr pg_cluster_);
 
-// userver::storages::postgres::ResultSet getLessons(
-//     userver::storages::postgres::ClusterPtr pg_cluster_);
+std::vector<Lesson> getLessons(
+    userver::storages::postgres::ClusterPtr pg_cluster_);
 
-// userver::storages::postgres::ResultSet getLessonByID(
-//     const std::string& id, userver::storages::postgres::ClusterPtr
-//     pg_cluster_);
+std::optional<Lesson> getLessonByID(
+    const std::string& id, userver::storages::postgres::ClusterPtr
+    pg_cluster_);
 
-// userver::storages::postgres::ResultSet updateLessonByID(
-//     const lms_service::LessonData& lesson_data,
-//     userver::storages::postgres::ClusterPtr pg_cluster_);
+std::optional<Lesson> updateLessonByID(
+    const lms_service::LessonData& lesson_data,
+    userver::storages::postgres::ClusterPtr pg_cluster_);
 
-// userver::storages::postgres::ResultSet deleteLessonByID(
-//     const std::string& id, userver::storages::postgres::ClusterPtr
-//     pg_cluster_);
+std::optional<std::string> deleteLessonByID(
+    const std::string& id, userver::storages::postgres::ClusterPtr
+    pg_cluster_);
 }  // namespace lesson_catalog_controller
 }  // namespace lms_service
