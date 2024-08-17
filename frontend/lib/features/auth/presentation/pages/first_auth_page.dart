fd7a7b7610993c11c1fc7f9ae3b046a0f3563522
Trushkova Mary <94931275+truefalsemary@users.noreply.github.com>
2024-08-17 16:06:46 +0300

    fix: colors for auth pages

--- frontend/lib/features/auth/presentation/pages/first_auth_page.dart
+++ frontend/lib/features/auth/presentation/pages/first_auth_page.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:lms_front/features/auth/presentation/widgets/auth_button.dart';
 import 'package:lms_front/features/auth/presentation/widgets/form_wrapper.dart';
+import 'package:lms_front/ui_kit/colors/color_palette.dart';
 import 'package:lms_front/ui_kit/typography/app_text_theme.dart';
 
-class FistAuthPage extends StatelessWidget {
-  const FistAuthPage({super.key});
+class FirstAuthPage extends StatelessWidget {
+  const FirstAuthPage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -44,9 +45,17 @@ class _AuthFormState extends State<_AuthForm> {
             decoration: InputDecoration(labelText: 'Логин'),
           ),
           const SizedBox(height: 1.5 * heightBetweenElements),
-          AuthButton(inputText: 'Продолжить', onPressed: () => ()),
+          AuthButton(
+            inputText: 'Продолжить',
+            onPressed: () => (),
+            backgroundColor: ColorPalette.green,
+          ),
           const SizedBox(height: heightBetweenElements),
-          AuthButton(inputText: 'Зарегистрироваться', onPressed: () => ()),
+          AuthButton(
+            inputText: 'Зарегистрироваться',
+            onPressed: () => (),
+            backgroundColor: ColorPalette.blue,
+          ),
         ],
       ),
     );
--- frontend/lib/features/auth/presentation/widgets/auth_button.dart
+++ frontend/lib/features/auth/presentation/widgets/auth_button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lms_front/ui_kit/colors/color_palette.dart';
 import 'package:lms_front/ui_kit/typography/app_text_theme.dart';
 
 class AuthButton extends StatelessWidget {
@@ -7,7 +8,7 @@ class AuthButton extends StatelessWidget {
     super.key,
     this.onPressed,
     this.foregroundColor = Colors.white,
-    this.backgroundColor = Colors.black,
+    this.backgroundColor = ColorPalette.blue,
   });
 
   final String inputText;
