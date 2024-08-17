460126cc954d12d0aa0697c04669d44fb96fca79
eagerbeaver04 <mike.markov04@gmail.com>
2024-08-17 12:36:25 +0300

    feat: add migrations for super users with email super_user@super.ru and password super

---
+++ service/pg_service_template/postgres/migrations/1.0.0/V002_add_super_user.sql
@@ -0,0 +1,2 @@
+INSERT INTO USERS (current_user_type, user_email, user_password, user_name, user_surname, user_middle_name) 
+VALUES('super', 'super_user@super.ru', 'super', 'SuperVasya', 'Pupkin', 'Ivanovich')
